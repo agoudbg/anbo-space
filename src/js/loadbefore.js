db1bfd412982aaf278047f579fa056ac3f535dd6
agoudbg <55237525+agoudbg@users.noreply.github.com>
2021-02-07 23:28:14 +0800

    开发进度保存

---
+++ src/js/loadbefore.js
@@ -0,0 +1,19 @@
+// 判断debug
+DEBUG_URLS = ["localhost", "t.anbo.space", "sakura"];
+debug = false;
+for (i = 0; i <= DEBUG_URLS.length; i++) {
+    if (window.location.host.match(DEBUG_URLS[i]) != null)
+        debug = true;
+}
+
+HTTP_PAGE = ["live"];
+// 跳转https
+if (debug = false) {
+    for (i = 0; i <= HTTP_PAGE.length; i++) {
+        if ((window.location.pathname.match(HTTP_PAGE[i]) != null) & (window.location.protocol == "https:"))
+            window.location.href = "http://" + window.location.host + window.location.pathname + window.location.search;
+        else if (window.location.protocol == "https:")
+            window.location.href = "https://" + window.location.host + window.location.pathname + window.location.search;
+
+    }
+}
