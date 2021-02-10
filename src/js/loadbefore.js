209a42a58e58a0ccf1dedb65d69ed6be05c60d0b
agoudbg <55237525+agoudbg@users.noreply.github.com>
2021-02-10 14:28:04 +0800

    重写http和https之间跳转判断逻辑

--- src/js/loadbefore.js
+++ src/js/loadbefore.js
@@ -9,12 +9,17 @@ for (i = 0; i < DEBUG_URLS.length; i++) {
 HTTP_PAGE = ["live", "tv-desk", "chunwanlive"];
 // 跳转https
 if (debug == false) {
+    FORCEHTTP = false;
     for (i = 0; i < HTTP_PAGE.length; i++) {
-        if ((window.location.pathname.match(HTTP_PAGE[i]) != null) & (window.location.protocol == "https:"))
-            window.location.href = "http://" + window.location.host + window.location.pathname + window.location.search;
-        else if (window.location.protocol == "http:")
-            window.location.href = "https://" + window.location.host + window.location.pathname + window.location.search;
-
+        if ((window.location.pathname.match(HTTP_PAGE[i]) != null) & (window.location.protocol == "https:")) {
+            FORCEHTTP = true;
+            break;
+        }
     }
+    if (window.location.protocol == "http:" & FORCEHTTP == false)
+        window.location.href = "https://" + window.location.host + window.location.pathname + window.location.search;
+    if (window.location.protocol == "https:" & FORCEHTTP == true)
+        window.location.href = "http://" + window.location.host + window.location.pathname + window.location.search;
+
 
 }
