6919a946e3adfbe2aebfd91881e3f17a0d41b042
agoudbg <55237525+agoudbg@users.noreply.github.com>
2021-02-10 12:31:42 +0800

    开发进度保存

--- src/js/loadbefore.js
+++ src/js/loadbefore.js
@@ -1,20 +1,20 @@
 // 判断debug
 DEBUG_URLS = ["localhost", "t.anbo.space", "sakura"];
 debug = false;
-for (i = 0; i <= DEBUG_URLS.length; i++) {
+for (i = 0; i < DEBUG_URLS.length; i++) {
     if (window.location.host.match(DEBUG_URLS[i]) != null)
         debug = true;
 }
 
-HTTP_PAGE = ["live"];
+HTTP_PAGE = ["live", "tv-desk", "chunwanlive"];
 // 跳转https
-if (debug = false) {
-    for (i = 0; i <= HTTP_PAGE.length; i++) {
+if (debug == false) {
+    for (i = 0; i < HTTP_PAGE.length; i++) {
         if ((window.location.pathname.match(HTTP_PAGE[i]) != null) & (window.location.protocol == "https:"))
             window.location.href = "http://" + window.location.host + window.location.pathname + window.location.search;
     }
-        if (window.location.protocol == "http:")
-            window.location.href = "https://" + window.location.host + window.location.pathname + window.location.search;
+    if (window.location.protocol == "http:")
+        window.location.href = "https://" + window.location.host + window.location.pathname + window.location.search;
 
 
 }
