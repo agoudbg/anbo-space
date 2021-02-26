c073a27103e7d72b9d87732b98e7bb8ced07b767
agoudbg <55237525+agoudbg@users.noreply.github.com>
2021-02-26 09:28:22 +0800

    测试央视频积分计算器

--- apps/yspcal/yspcal.js
+++ apps/yspcal/yspcal.js
@@ -75,11 +75,13 @@ function calc() {
 	var nowDate = new Date();
 	if (date.getFullYear() - nowDate.getFullYear() > 0)
 		warningExpire = true;
+	else
+		warningExpire = false;
 	resultbox.innerHTML += "根据你的所填，如果你从今天起";
 	if (workingDates == 30.5)
 		resultbox.innerHTML += "每";
 	else
-		resultbox.innerHTML += "平均每月 " + workingDates+" ";
+		resultbox.innerHTML += "平均每月 " + workingDates + " ";
 	resultbox.innerHTML += "天在央视频：<br />• 签到";
 	if (videoPerDayInbox.value != 0)
 		resultbox.innerHTML += "<br />• 观看视频 " + videoPerDayInbox.value + " 次  ";
@@ -91,21 +93,21 @@ function calc() {
 		resultbox.innerHTML += "<br />• 关注央视频号 " + followPerDayInbox.value + " 次  ";
 	if (otherInbox.value != 0)
 		resultbox.innerHTML += "，<br />并且每日通过其他途径获取或消耗 " + otherInbox.value + " 积分";
-	resultbox.innerHTML += "，那么你将在" 
-	;
+	resultbox.innerHTML += "，那么你将在"
+		;
 	if (actuallyDay != 0)
 		resultbox.innerHTML += "约 " + actuallyDay + " 天后，即 " + finishDate;
 	else
 		resultbox.innerHTML += "今天";
 	resultbox.innerHTML += "达成获得 " + wishInbox.value + " 积分的目标。安播空间祝你如愿以偿。";
-	if (warningExpire==true)
-	resultbox.innerHTML += "<br />提示：此段时间跨过年末，可能产生积分过期。";
+	if (warningExpire == true)
+		resultbox.innerHTML += "<br />提示：此段时间跨过年末，可能产生积分过期。";
 }
 
 function copy() {
 	let input = document.createElement('input')
 	input.setAttribute('readonly', 'readonly') // 防止手机上弹出软键盘
-	input.setAttribute('value', "安播空间的央视频积分计算器告诉我，"+resultbox.innerHTML.replace(/<br>/g, "").replace(/你/g, "我")+"你也快到 https://anbo.space/apps/yspcal 算算吧！") // txval 为所需复制的值 变量 或者 写死
+	input.setAttribute('value', "安播空间的央视频积分计算器告诉我，" + resultbox.innerHTML.replace(/<br>/g, "").replace(/你/g, "我") + "你也快到 https://anbo.space/apps/yspcal 算算吧！") // txval 为所需复制的值 变量 或者 写死
 	document.body.appendChild(input)
 	input.select()
 	document.execCommand('copy')
